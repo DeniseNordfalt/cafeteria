7a5a651242186cdfe991aadb18f4ea5fb6d23921
milkon <melkon_2010@hotmail.com>
2021-09-27 12:16:23 +0200

    printing transactions on form button click

--- app.js
+++ app.js
@@ -6,26 +6,58 @@ const coffees = [
   { name: 'Latte', price: 40 },
 ];
 
-const inputForm = document.getElementById('input-form');
+const membershipStatus = [
+  {
+    level: 'Brons',
+    minCups: 1,
+    maxCups: 9,
+  },
+  {
+    level: 'Silver',
+    minCups: 10,
+    maxCups: 29,
+  },
+  {
+    level: 'Guld',
+    minCups: 30,
+  },
+];
 
+const transactions = [];
+
+const inputForm = document.getElementById('input-form');
 const coffeeSort = document.getElementById('coffee-menu');
 const numberOfCoffeeCups = document.getElementById('number-of-cups');
-
 const formSubmitButton = document.getElementById('form-button');
+const transactionsSection = document.getElementById('transactions-section');
 
-const f = function (e) {
+const btnOnClick = function (e) {
   e.preventDefault();
-  console.log(coffeeSort.value, numberOfCoffeeCups.value);
   console.log(createTransaction());
   inputForm.reset();
+  console.log(...transactions);
+};
+
+const saveTranaction = function () {
+  const transaction = {
+    coffee: coffees[coffeeSort.value].name,
+    quantity: numberOfCoffeeCups.value,
+  };
+  transactions.push(transaction);
 };
 
 const createTransaction = function () {
-  return `Du köpte ${numberOfCoffeeCups.value} st ${
+  saveTranaction();
+  const transactionText = `Du köpte ${numberOfCoffeeCups.value} st ${
     coffees[coffeeSort.value].name
   } för ${coffees[coffeeSort.value].price} kr styck. Summa: ${
     numberOfCoffeeCups.value * coffees[coffeeSort.value].price
   }  `;
+  const transaction = document.createElement('p');
+  transaction.innerText = transactionText;
+  transaction.classList.add('transaction');
+  transactionsSection.appendChild(transaction);
+  return transactionText;
 };
 
-formSubmitButton.addEventListener('click', f);
+formSubmitButton.addEventListener('click', btnOnClick);
