ac0841ecfaa7713694410e192acd180ec227b3f3
milkon <melkon_2010@hotmail.com>
2021-09-27 11:11:09 +0200

    added eventlistener to form-button onclick to print a recipe

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-"use strict";
+'use strict';
+
+const coffees = [
+  { name: 'Brygg Kaffe', price: 20 },
+  { name: 'Cappucino', price: 30 },
+  { name: 'Latte', price: 40 },
+];
+
+const inputForm = document.getElementById('input-form');
+
+const coffeeSort = document.getElementById('coffee-menu');
+const numberOfCoffeeCups = document.getElementById('number-of-cups');
+
+const formSubmitButton = document.getElementById('form-button');
+
+const f = function (e) {
+  e.preventDefault();
+  console.log(coffeeSort.value, numberOfCoffeeCups.value);
+  console.log(createTransaction());
+  inputForm.reset();
+};
+
+const createTransaction = function () {
+  return `Du köpte ${numberOfCoffeeCups.value} st ${
+    coffees[coffeeSort.value].name
+  } för ${coffees[coffeeSort.value].price} kr styck. Summa: ${
+    numberOfCoffeeCups.value * coffees[coffeeSort.value].price
+  }  `;
+};
+
+formSubmitButton.addEventListener('click', f);
